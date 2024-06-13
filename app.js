7aead775dcb4fc993dd6f601966c7bf1a3f84edb
Rudy Ajuchán <rudyajuchansec32@gmail.com>
2024-06-13 16:49:26 -0600

    api listo

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors');
 const dbConfig = require('./config/db.config');
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 // Configuración de la base de datos
@@ -19,8 +21,8 @@ db.connect(err => {
 app.use(express.json());
 
 // Rutas
-app.get('/usuarios', (req, res) => {
-    db.query('SELECT * FROM usuarios', (err, results) => {
+app.get('/empleados', (req, res) => {
+    db.query('SELECT * FROM empleado WHERE estado=1', (err, results) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
@@ -29,9 +31,9 @@ app.get('/usuarios', (req, res) => {
 });
 
 // Obtener un usuario por ID
-app.get('/usuarios/:id', (req, res) => {
+app.get('/empleados/:id', (req, res) => {
     const userId = req.params.id;
-    db.query('SELECT * FROM usuarios WHERE id = ?', [userId], (err, results) => {
+    db.query('SELECT * FROM empleado WHERE id = ? AND estado=1', [userId], (err, results) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
@@ -43,34 +45,34 @@ app.get('/usuarios/:id', (req, res) => {
 });
 
 // Crear un nuevo usuario
-app.post('/usuarios', (req, res) => {
-    const { nombre, email } = req.body;
-    db.query('INSERT INTO usuarios (nombre, email) VALUES (?, ?)', [nombre, email], (err, results) => {
+app.post('/empleados', (req, res) => {
+    const { nombre, apellido, dpi, estado_civil, sueldo_mensual, descuentos, saldo_liquido } = req.body;
+    db.query('INSERT INTO empleado (nombre, apellido, dpi, estado_civil, sueldo_mensual, descuentos, saldo_liquido, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, NOW(), NOW())', [nombre, apellido, dpi, estado_civil, sueldo_mensual, descuentos, saldo_liquido], (err, results) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
-        res.status(201).json({ id: results.insertId, nombre, email });
+        res.status(201).json({ id: results.insertId, nombre, apellido, dpi, estado_civil, sueldo_mensual, descuentos, saldo_liquido });
     });
 });
 
-app.put('/usuarios/:id', (req, res) => {
+app.put('/empleados/:id', (req, res) => {
     const userId = req.params.id;
-    const { nombre, email } = req.body;
-    db.query('UPDATE usuarios SET nombre = ?, email = ? WHERE id = ?', [nombre, email, userId], (err, results) => {
+    const { nombre, apellido, dpi, estado_civil, sueldo_mensual, descuentos, saldo_liquido } = req.body;
+    db.query('UPDATE empleado SET nombre = ?, apellido = ?, dpi = ?, estado_civil = ?, sueldo_mensual = ?, descuentos = ?, saldo_liquido = ?, updated_at = now() WHERE id = ?', [nombre, apellido, dpi, estado_civil, sueldo_mensual, descuentos, saldo_liquido, userId], (err, results) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
         if (results.affectedRows === 0) {
             return res.status(404).json({ error: 'Usuario no encontrado' });
         }
-        res.json({ id: userId, nombre, email });
+        res.json({ id: userId, nombre, apellido, dpi, estado_civil, sueldo_mensual, descuentos, saldo_liquido });
     });
 });
 
 // Eliminar un usuario
-app.delete('/usuarios/:id', (req, res) => {
+app.delete('/empleados/:id', (req, res) => {
     const userId = req.params.id;
-    db.query('DELETE FROM usuarios WHERE id = ?', [userId], (err, results) => {
+    db.query('UPDATE empleado SET estado = 0, updated_at = now() WHERE id = ?', [userId], (err, results) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     host: 'localhost',
     user: 'clusteradmin',
+    port: '6446',
     password: 'admin',
     database: 'replica'
 };
